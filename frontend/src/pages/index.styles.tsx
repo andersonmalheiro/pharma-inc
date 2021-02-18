e18054aeb42aac2a61ccaede3b543768f21821b4
Anderson Malheiro de Carvalho <anderson.malheiro@aluno.fapce.edu.br>
2021-02-18 20:34:56 -0300

    Updating main page

---
+++ frontend/src/pages/index.styles.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const StyledMain = styled.div`
+  padding: 10%;
+  height: calc(100vh - 70px);
+`;
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,17 +1,58 @@
 import Head from 'next/head';
-import { Navbar } from 'components';
+import { Navbar, Table } from 'components';
+import { StyledMain } from './index.styles';
+
+const columns = [
+  {
+    title: 'Name',
+    key: 'name',
+  },
+  {
+    title: 'Gender',
+    key: 'gender',
+  },
+  {
+    title: 'Birth',
+    key: 'birth',
+  },
+];
+
+const data = [
+  {
+    name: 'José',
+    gender: 'male',
+    birth: '22/12/1998'
+  },
+  {
+    name: 'José',
+    gender: 'male',
+    birth: '22/12/1998'
+  },
+  {
+    name: 'José',
+    gender: 'male',
+    birth: '22/12/1998'
+  },
+  {
+    name: 'José',
+    gender: 'male',
+    birth: '22/12/1998'
+  },
+];
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>Create Next App</title>
+        <title>Pharma Inc.</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <Navbar />
 
-      <main></main>
+      <StyledMain>
+        <Table columns={columns} data={data} />
+      </StyledMain>
     </div>
   );
 }
